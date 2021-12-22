d72b1e28d05b675fb6e082c8d42d0faf8a57da76
Michael Cohen <miccoh1994@gmail.com>
2021-12-22 16:12:52 +0200

    initial commit

---
+++ apps/test/src/app/app.tsx
@@ -0,0 +1,13 @@
+import styles from './app.module.scss';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="test" />
+      <div />
+    </>
+  );
+}
+
+export default App;
